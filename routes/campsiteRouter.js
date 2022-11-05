d21aa2eb6da40bd948b41055da9d07ddf61cbb86
Bryan <blcolley@me.com>
2022-11-05 12:48:41 -0400

    workshop 1 completion

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -27,4 +27,32 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites')
 });
 
+
+campsiteRouter.route('/:campsiteId')
+
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end(`Will send details of the camnpsite: ${req.params.campsiteId}`);
+})
+
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /campsites');
+})
+
+.put((req, res) => {
+    res.end(`Updating campsite with new name: ${req.body.name} 
+    and description: ${req.body.description}`);
+})
+
+.delete((req, res) => {
+    res.end(`Deleting a campsite with id: ${req.params.campsiteId}`)
+});
+
+
 module.exports = campsiteRouter;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const res = require('express/lib/response');
 const morgan = require('morgan')
 const campsiteRouter = require('./routes/campsiteRouter');
+const partnerRouter = require('./routes/partnerRouter');
+const promotionRouter = require('./routes/promotionRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,7 +13,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
-
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
